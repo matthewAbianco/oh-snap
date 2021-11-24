7148bb9b3ffd2e8aca353a999b6a33442a9902c9
matthewAbianco <matthewbianco@hotmail.com>
2021-11-24 12:42:25 -0500

    added props to the nav bar

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import About from './components/about/About';
+import Gallery from './components/gallery/Gallery';
 import Nav from './components/nav/Nav';
+import React, { useState } from 'react';
 
 function App() {
+
+
+const [categories] = useState([
+  { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects'},
+  { name: 'portraits', description: 'Portraits of people in my life' },
+  { name: 'food', description: 'Delicious delicacies' },
+  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+]);
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div className="app">
-      <Nav/>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      />
       <main>
+        <Gallery/>
         <About/>
         </main>
     </div>
