179328e400cb29c9d9a08765031340a88e41c3d2
matthewAbianco <matthewbianco@hotmail.com>
2021-11-24 13:07:13 -0500

    finished gallery

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,31 +1,32 @@
-import './App.css';
-import About from './components/about/About';
-import Gallery from './components/gallery/Gallery';
-import Nav from './components/nav/Nav';
 import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
 
-
-const [categories] = useState([
-  { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects'},
-  { name: 'portraits', description: 'Portraits of people in my life' },
-  { name: 'food', description: 'Delicious delicacies' },
-  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-]);
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
-    <div className="app">
+    <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      />
+      ></Nav>
       <main>
-        <Gallery/>
-        <About/>
-        </main>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
+      </main>
     </div>
   );
 }
